8ad8d93845a7a48ec399252a1222e93ed21b039e
ShadowWolf90 <lech696@gmail.com>
2023-12-18 23:08:16 +0100

    Some basic stuff

--- lua/gameinstructor/gameinstructor_server.lua
+++ lua/gameinstructor/gameinstructor_server.lua
@@ -1,1 +1,29 @@
 print("[Game Instructor [SERVER]] Loaded.")
+
+util.AddNetworkString("GINetworkPosition")
+util.AddNetworkString("GINetworkEntity")
+
+function CreateGIHint(icon, text, pos)
+    if IsValid(GIIcons[icon]) and isentity(pos) and text != nil then
+        pos:SetNWString("GIIcon", icon)
+        pos:SetNWString("GIText", text)
+    elseif IsValid(GIIcons[icon]) and isvector(pos) and text != nil then
+        net.Start("GINetwork")
+        net.WriteString(icon)
+        net.WriteString(text)
+        net.WriteVector(pos)
+        net.Broadcast()
+    elseif (not IsValid(GIIcons[icon]) or text == nil or pos == nil) then
+        if not IsValid(GIIcons[icon]) then
+            --ErrorNoHalt("Icon, Text or Position/Entity is incorrect. Please check your LUA script.")
+            ErrorNoHalt("Icon is incorrect. Please check your LUA script.")
+        elseif text == nil then
+            ErrorNoHalt("Text is incorrect. Please check your LUA script.")
+        elseif pos == nil then
+            ErrorNoHalt("Position/Entity is incorrect. Please check your LUA script.")
+        end
+    end
+end
+
+
+
